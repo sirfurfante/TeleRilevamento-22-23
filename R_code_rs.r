741edf96eb374b7b5e76197139c617a58b81b2fd
sirfurfante <114928515+sirfurfante@users.noreply.github.com>
2022-10-05 17:19:50 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -8,4 +8,12 @@ setwd("C:/lab/") #windows
 #richiamo le immagini con la funzione brich
 #import
 l2011 <- brich("p224r63_2011.grd")
+l2011
+#faccio il plot dell'immagine
+plot(l2011)
+#colorRampPalette per editare la legenda del plot
+
+colorRampPalette
+cl <- colorRampPalette(c("black","grey","light grey")) (100) #100 rappresentano il num di colori intermedi
+plot(l2011, col=cl)
 
