47220e1438122746fc2e3767e7189e189be39542
sirfurfante <114928515+sirfurfante@users.noreply.github.com>
2022-11-29 10:03:41 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -17,3 +17,70 @@ colorRampPalette
 cl <- colorRampPalette(c("black","grey","light grey")) (100) #100 rappresentano il num di colori intermedi
 plot(l2011, col=cl)
 
+
+# dev.off()
+
+# Landsat ETM+
+# b1 = blu
+# b2 = verde
+# b3 = rosso
+# b4 = infrarosso vicino NIR
+
+# plot della banda del blu - B1_sre
+plot(l2011$B1_sre) # trinity
+# or
+plot(l2011[[1]]) # neo
+
+plot(l2011$B1_sre) 
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011$B1_sre, col=cl) 
+
+# plot b1 from dark blue to blue to light blue
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(l2011$B1_sre, col=clb) 
+
+# let's export the image and let it appear in the lab folder: kind of magic!
+pdf("banda1.pdf")
+plot(l2011$B1_sre, col=clb) 
+dev.off()
+
+png("banda1.png")
+plot(l2011$B1_sre, col=clb) 
+dev.off()
+
+# plot b2 from dark green to green to light green
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(l2011$B2_sre, col=clg) 
+
+# multiframe
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb) 
+plot(l2011$B2_sre, col=clg) 
+dev.off()
+
+# export multiframe plot
+pdf("multiframe.pdf")
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb) 
+plot(l2011$B2_sre, col=clg) 
+dev.off()
+
+# exercise: revert the multiframe
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb) 
+plot(l2011$B2_sre, col=clg) 
+
+# let's plot the first four bands
+par(mfrow=c(2,2))
+# blue
+plot(l2011$B1_sre, col=clb) 
+# green
+plot(l2011$B2_sre, col=clg) 
+# red
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(l2011$B3_sre, col=clr)
+# NIR
+clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(l2011$B4_sre, col=clnir)
+#or
+plot(l2011[[4]])
