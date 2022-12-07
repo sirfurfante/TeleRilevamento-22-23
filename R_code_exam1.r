b4689d1cb078f83a6764489ec73d242f19ee56eb
sirfurfante <114928515+sirfurfante@users.noreply.github.com>
2022-12-07 11:03:37 +0100

    Update R_code_exam1.r

--- R_code_exam1.r
+++ R_code_exam1.r
@@ -14,13 +14,13 @@ plotRGB(K2016, r=1, g=2, b=3, stretch="lin")
 # layer 3 = green
 
 par(mfrow=c(2,1))
-plotRGB(k2015, r=1, g=2, b=3, stretch="lin")
-plotRGB(k2016, r=1, g=2, b=3, stretch="lin") 
+plotRGB(K2015, r=1, g=2, b=3, stretch="lin")
+plotRGB(K2016, r=1, g=2, b=3, stretch="lin") 
 
 # DVI Difference Vegetation Index pre fire, 2015
-dvi2015 = k2015[[1]] - k2015[[2]]
+dvi2015 = K2015[[1]] - K2015[[2]]
 # or:
-# dvik2015 = k2015$Kamchatka_Pre_2015.1 - k2015$Kamchatka_Pre_2015.2
+# dviK2015 = k2015$Kamchatka_Pre_2015.1 - k2015$Kamchatka_Pre_2015.2
 dvi2015
 
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
@@ -28,7 +28,7 @@ plot(dvi2015, col=cl)
 
 
 # DVI Difference Vegetation Index post fire, 2016
-dvi2016 = k2016[[1]] - k2016[[2]]
+dvi2016 = K2016[[1]] - K2016[[2]]
 # or:
 # dvik2016 = k2016$Kamchatka_Post_2016.1 - k2016$Kamchatka_Post_2016.2
 dvi2016
@@ -44,26 +44,26 @@ plot(dvi_dif, col=cld)
 # NDVI can be used to compare images with a different radiometric resolution
 # NDVI 2015
 
-#dvi2015 = k2015[[1]] - k2015[[2]]
+#dvi2015 = K2015[[1]] -K2015[[2]]
 ndvi2015 = dvi2015 / (k2015[[1]] + k2015[[2]])
 # or
-ndvi2015 = (k2015[[1]] - k2015[[2]]) / (k2015[[1]] + k2015[[2]])
+ndvi2015 = (K2015[[1]] - K2015[[2]]) / (K2015[[1]] + K2015[[2]])
 
 # Multiframe with plotRGB on top of the NDVI image
 par(mfrow=c(2,1))
-plotRGB(k2015, r=1, g=2, b=3, stretch="lin")
+plotRGB(K2015, r=1, g=2, b=3, stretch="lin")
 plot(ndvi2015, col=cl)
 
 # NDVI 2016
 
-#dvi2016 = k2016[[1]] - k2016[[2]]
-ndvi2016 = dvi2016 / (k2016[[1]] + k2016[[2]])
+#dvi2016 = K2016[[1]] - K2016[[2]]
+ndvi2016 = dvi2016 / (K2016[[1]] + K2016[[2]])
 # or
-ndvi2016 = (k2016[[1]] - k2016[[2]]) / (k2016[[1]] + k2016[[2]])
+ndvi2016 = (K2016[[1]] - K2016[[2]]) / (K2016[[1]] + K2016[[2]])
 
 # Multiframe with plotRGB on top of the NDVI image
 par(mfrow=c(2,1))
-plotRGB(k2016, r=1, g=2, b=3, stretch="lin")
+plotRGB(K2016, r=1, g=2, b=3, stretch="lin")
 plot(ndvi2016, col=cl)
 
 # Multiframe with NDVI2015 on top of the NDVI2016 image
@@ -72,8 +72,8 @@ plot(ndvi2015, col=cl)
 plot(ndvi2016, col=cl)
 
 # Automatic spectral indices by the spectralIndices function
-si2015 <- spectralIndices(k2015, green=3, red=2, nir=1)
-si2016 <- spectralIndices(k2016, green=3, red=2, nir=1)
+si2015 <- spectralIndices(K2015, green=3, red=2, nir=1)
+si2016 <- spectralIndices(K2016, green=3, red=2, nir=1)
 
 par(mfrow=c(2,1))
 plot(si2015, col=cl)
