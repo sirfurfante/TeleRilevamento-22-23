59df60c34a3b04358ba1d2b73a82bc383afffbce
Nicola Marchi <114928515+sirfurfante@users.noreply.github.com>
2023-02-03 08:45:57 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -39,76 +39,6 @@ plot(nbr07_2016, col=cl, main="NBR 2016 - 07")
 plot(nbr09_2016, col=cl, main="NBR 2016 - 09")
 
 
-# maggio <- brick("17_05_2016_Kachatka.jpg")
-# luglio <- brick("4_7_2016_Kamchatka.jpg")
-# settembre <- brick("6_9_2016_Kamchatka.jpg")
-# layer 1 = NIR
-# layer 2 = red
-# layer 3 = green
-
-# par(mfrow=c(1,3))
-# plotRGB(maggio, r=1, g=2, b=3, stretch="lin", main="05-2016")
-# plotRGB(luglio, r=1, g=2, b=3, stretch="lin", main="07-2016") 
-# plotRGB(settembre, r=1, g=2, b=3, stretch="lin", main="09-2016")
-
-
-# DVI Difference Vegetation Index pre fire, 2016 (maggio)
-# dvi_pre = maggio[[1]] - maggio[[2]]
-# dvi_pre
-# cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
-# plot(dvi_pre, col=cl)
-
-
-# DVI Difference Vegetation Index post fire, 2016 (luglio)
-#dvi_post = luglio[[1]] - luglio[[2]]
-#dvi_post
-#plot(dvi_post, col=cl)
-
-# DVI Difference Vegetation Index post fire, 2016 (settembre)
-#dvi_post1 = settembre[[1]] - settembre[[2]]
-#dvi_post1
-#plot(dvi_post1, col=cl)
-
-# DVI difference in time
-#dvi_dif = dvi_pre - dvi_post
-#cld <- colorRampPalette(c('blue','white','red'))(100) 
-#plot(dvi_dif, col=cld)
-
-#dvi_dif1 = dvi_pre - dvi_post1
-#cld <- colorRampPalette(c('blue','white','red'))(100) 
-#plot(dvi_dif1, col=cld)
-
-# NDVI can be used to compare images with a different radiometric resolution
-# NDVI pre fire
-
-#ndvi_pre = dvi_pre / (maggio[[1]] + maggio[[2]])
-
-# Multiframe with plotRGB on top of the NDVI image
-#par(mfrow=c(2,1))
-#plotRGB(maggio, r=1, g=2, b=3, stretch="lin")
-#plot(ndvi_pre, col=cl)
-
-# NDVI post fire
-#ndvi_post = dvi_post / (luglio[[1]] + luglio[[2]])
-# Multiframe with plotRGB on top of the NDVI image
-#par(mfrow=c(1,2))
-#plotRGB(luglio, r=1, g=2, b=3, stretch="lin")
-#plotRGB(maggio, r=1, g=2, b=3, stretch="lin")
-#plot(ndvi_post, col=cl)
-
-# NDVI post fire 1
-#ndvi_post1 = dvi_post1 / (settembre[[1]] + settembre[[2]])
-# Multiframe with plotRGB on top of the NDVI image
-#par(mfrow=c(2,1))
-#plotRGB(settembre, r=1, g=2, b=3, stretch="lin")
-#plot(ndvi_post1, col=cl)
-
-# Multiframe with NDVI2015 on top of the NDVI2016 image
-#par(mfrow=c(3,1))
-#plot(ndvi_pre, col=cl, main="NDVI maggio")
-#plot(ndvi_post, col=cl, main="NDVI luglio")
-#plot(ndvi_post1, col=cl, main="NDVI settembre")
-
 #NDVI mediante lo studio delle bande B5(infrarosso) e B4(rosso)
 #B5 già caricato per il calcolo del NBR
 B405_2016 <- brick("B4_maggio.tif") #rosso
@@ -182,8 +112,8 @@ prop2 <- freq(post$map) / s2
 
 # build a dataframe
 cover <- c("Foresta","Nuvole","Incendio+Ombre")
-percent_2015 <- c(0.75, 0.11, 0.12)
-percent_2016 <- c(0.36, 0.12, 0.51)
+percent_2015 <- c(75, 11, 12)
+percent_2016 <- c(36, 12, 51)
 
 percentages <- data.frame(cover, percent_2015, percent_2016)
 percentages
